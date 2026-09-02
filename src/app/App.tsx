d3f04320ea5d3507dc13d3a4a78a71ac0b2269d1
Росин Семён <semen.rosin1@gmail.com>
2026-09-02 21:18:42 +0300

    fix: render app router from application root

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,15 @@
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+
+import { AppRouter } from './router/AppRouter';
 import { store } from './store/store';
 
-const App = () => {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  );
-};
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <AppRouter />
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
